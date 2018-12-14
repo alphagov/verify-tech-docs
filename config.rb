ac90f84cab207b62a0f379742a7316d75a82391e
Andrea Szöllössi <andrea.szollossi@digital.cabinet-office.gov.uk>
2018-12-14 15:35:39 +0000

    Change e-mail addres for 'Report problem'

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@ require 'govuk_tech_docs'
 
 GovukTechDocs::SourceUrls.class_eval do
   def report_issue_url
-    'mailto:helpemepls@gov.uk'
+    'mailto:idasupport@digital.cabinet-office.gov.uk'
   end
 end
 
